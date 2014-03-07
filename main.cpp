f3d0acb62e03e054c23714a6789388bd711ff6b2
root <root@dtcube01.nvidia.com>
2014-03-06 22:48:54 -0800

    fixed

--- main.cpp
+++ main.cpp
@@ -342,8 +342,8 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"nodegpucoin1.gpucoin.net", "nodegpucoin2.gpucoin.net", "nodegpucoin3.gpucoin.net", "nodegpucoin4.gpucoin.net", "nodegpucoin5.gpucoin.net"};
-static const string testnet_seeds[] = {"nodegpucoin1.gpucoin.net", "nodegpucoin2.gpucoin.net", "nodegpucoin3.gpucoin.net", "nodegpucoin4.gpucoin.net", "nodegpucoin5.gpucoin.net"};
+static const string mainnet_seeds[] = {"nodegpucoin1.realstackcoin.org", "nodegpucoin2.realstackcoin.org", "nodegpucoin3.realstackcoin.org", "nodegpucoin4.realstackcoin.org", "nodegpucoin5.realstackcoin.org"};
+static const string testnet_seeds[] = {"nodegpucoin1.realstackcoin.org", "nodegpucoin2.realstackcoin.org", "nodegpucoin3.realstackcoin.org", "nodegpucoin4.realstackcoin.org", "nodegpucoin5.realstackcoin.org"};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
